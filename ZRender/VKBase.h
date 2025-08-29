80d8d8a4d7580dcde97b2a60fb8e326e9996df95
nausicaa <12635849+nausicav@user.noreply.gitee.com>
2025-08-30 01:10:58 +0800

    接入采样器对象

--- ZRender/VKBase.h
+++ ZRender/VKBase.h
@@ -2108,6 +2108,43 @@ namespace vulkan
         }
     };
 
+    class sampler
+    {
+        VkSampler handle = VK_NULL_HANDLE;
+
+    public:
+        sampler() = default;
+
+        sampler(VkSamplerCreateInfo& createInfo)
+        {
+            Create(createInfo);
+        }
+
+        sampler(sampler&& other) noexcept
+        {
+            MoveHandle;
+        }
+
+        ~sampler()
+        {
+            DestroyHandleBy(vkDestroySampler);
+        }
+
+        //Getter
+        DefineHandleTypeOperator;
+        DefineAddressFunction;
+        //Non-const Function
+        result_t Create(VkSamplerCreateInfo& createInfo)
+        {
+            // sampler 决定“如何采样”，而 image view 决定“采样哪一段图像资源”。
+            createInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+            VkResult result = vkCreateSampler(graphicsBase::Base().Device(), &createInfo, nullptr, &handle);
+            if (result)
+                outStream << std::format("[ sampler ] ERROR\nFailed to create a sampler!\nError code: {}\n", int32_t(result));
+            return result;
+        }
+    };
+
     class shaderModule
     {
         VkShaderModule handle = VK_NULL_HANDLE;
